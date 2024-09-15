f1236631bb91ecdaf90b9e20610d9019046b3549
igon <igon@users.noreply.github.com>
2024-09-15 13:41:31 -0400

    Langsmith setup

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import chainlit as cl
 import openai
 import os
+from prompts import SYSTEM_PROMPT
+from langsmith.wrappers import wrap_openai
 from dotenv import load_dotenv
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 endpoint_url = os.getenv("OPENAI_ENDPOINT_URL")    
-client = openai.AsyncClient(api_key=openai.api_key, base_url=endpoint_url)
+client = wrap_openai(openai.AsyncClient(
+    api_key=openai.api_key,
+    base_url=endpoint_url)
+)
 
 # https://platform.openai.com/docs/models/gpt-4o
 model_kwargs = {
@@ -16,16 +21,20 @@ model_kwargs = {
     "max_tokens": 500,
 }
 
+ENABLE_SYSTEM_PROMPT = True
+
+
 @cl.on_message
 async def on_message(message: cl.Message):
     # Maintain an array of messages to send to the LLM
     message_history = cl.user_session.get("message_history", [])
-    message_history.append({"role": "user", "content": message.content})
 
-    response_message = cl.Message(
-        content="",
-    )
-    await response_message.send()
+    if ENABLE_SYSTEM_PROMPT and (not message_history or message_history[0]["role"] != "system"):
+        system_prompt_content = SYSTEM_PROMPT
+        message_history.insert(0, {"role": "system", "content": system_prompt_content}) 
+
+    #add user message to the history
+    message_history.append({"role": "user", "content": message.content})
 
     # https://platform.openai.com/docs/guides/chat-completions/response-forma
     stream = await client.chat.completions.create(
@@ -34,6 +43,10 @@ async def on_message(message: cl.Message):
         stream=True,
     )
     
+    response_message = cl.Message(
+    content="", 
+    )
+
     # https://platform.openai.com/docs/guides/chat-completions/response-format
     async for chunk in stream:
         if chunk := chunk.choices[0].delta.content or "":
@@ -44,3 +57,6 @@ async def on_message(message: cl.Message):
     # Add the LLM response to the message history
     message_history.append({"role": "assistant", "content": response_message.content})
     cl.user_session.set("message_history", message_history)
+
+
+
